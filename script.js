e604ea9680959468a75357fa6e431cee80e20fe5
xxfsd <josh.fsd@protonmail.com>
2022-05-21 19:25:47 +1000

    Create unique button classes for numbers and operators

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberInput = document.querySelectorAll('.buttonContainer');
+
+const addFunction = (sum1, sum2) => sum1 + sum2;
+const subtractFunction = (sum1, sum2) => sum1 - sum2;
+const multiplyFunction = (sum1, sum2) => sum1 * sum2;
+const divideFunction = (sum1, sum2) => sum1 / sum2;
+
