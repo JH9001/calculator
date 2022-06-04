2a1d71d79cd5bd96b89f8a373e55094588595ae7
xxfsd <josh.fsd@protonmail.com>
2022-06-04 13:11:18 +1000

    Implemented percentage button, working on decimal input

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const numberInput = document.querySelectorAll('.number');
 const operatorInput = document.querySelectorAll('.operator');
 const equalsInput = document.querySelector('.equals');
 const clearInput = document.querySelector('.clear');
+
 const plusMinusInput = document.querySelector('.plusMinus');
 const percentageInput = document.querySelector('.percentage');
 const decimalInput = document.querySelector('.decimal');
@@ -22,6 +23,8 @@ const divideFunction = (sum1, sum2) => sum1 / sum2;
 const multiplyFunction = (sum1, sum2) => sum1 * sum2;
 const subtractFunction = (sum1, sum2) => sum1 - sum2;
 const addFunction = (sum1, sum2) => Number(sum1) + Number(sum2);
+const percentFunction = (sum1) => (sum1/100);
+const plusMinusFunction = (sum1) => (sum1 * -1);
 
 //saves number input to variables depending on operatorSelect variable value
 numberInput.forEach(num => {
@@ -52,6 +55,21 @@ operatorInput.forEach(operator => {
   });
 });
 
+plusMinusInput.addEventListener('click', () => {
+  if (firstSum !== '' && secondSum == '') {
+  firstSum = plusMinusFunction(firstSum);
+  numberDisplay.textContent = firstSum;
+  } else {
+    secondSum = plusMinusFunction(secondSum);
+    numberDisplay.textContent = secondSum;
+  }
+});
+
+percentageInput.addEventListener('click', () => {
+  firstSum = percentFunction(firstSum);
+  numberDisplay.textContent = firstSum;
+});
+
 //saves the operator clicked into the operatorSelect variable
 divideInput.addEventListener('click', () => {
   operatorSelect = divideInput.textContent;
@@ -75,23 +93,29 @@ addInput.addEventListener('click', () => {
 
 //uses operator in operatorSelect variable and invokes maths function
 equalsInput.addEventListener('click', () => {
-  if (operatorSelect == '/') {
+  if (operatorSelect == '/' && secondSum !== '') {
       firstSum = divideFunction(firstSum, secondSum);
-  } else if (operatorSelect == '*') {
+  } else if (operatorSelect == '*' && secondSum !== '') {
       firstSum = multiplyFunction(firstSum, secondSum);
-  } else if (operatorSelect == '-') {
+  } else if (operatorSelect == '-' && secondSum !== '') {
       firstSum = subtractFunction(firstSum, secondSum);
-  } else if (operatorSelect == '+') {
+  } else if (operatorSelect == '+' && secondSum !== '') {
       firstSum = addFunction(firstSum, secondSum);
   }
     numberDisplay.textContent = firstSum;
     secondSum = '';
-    operatorSelect = '';
 });
 
-plusMinusInput.addEventListener('click', () => {
-  if (firstSum !== '' && secondSum == '')
-    firstSum += '-';
+decimalInput.addEventListener('click', () => {
+  if (!firstSum.includes('.') && !secondSum.includes('.')) {
+    decimalInput.disabled = false;
+    firstSum += decimalInput.textContent;
+    numberDisplay.textContent = firstSum;
+  } else if (!secondSum.includes('.')) {
+    decimalInput.disabled = false;
+    secondSum += decimalInput.textContent;
+    numberDisplay.textContent = secondSum;
+  }
 });
 
 //clears the calculator display on click of C button
