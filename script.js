90deba27309b4d6b1e366141f8f3d75d7b622a0a
xxfsd <josh.fsd@protonmail.com>
2022-06-05 17:54:23 +1000

    Disabled display overflow with numberDisplay = sum.toString().slice(0,7); and created image directory with github logo

--- script.js
+++ script.js
@@ -31,10 +31,10 @@ numberInput.forEach(num => {
   num.addEventListener('click', () => {
     if (operatorSelect == '') {
       firstSum += num.textContent;
-      numberDisplay.textContent = firstSum;
+      numberDisplay.textContent = firstSum.toString().slice(0,7);
     } else {
       secondSum += num.textContent;
-      numberDisplay.textContent = secondSum;
+      numberDisplay.textContent = secondSum.toString().slice(0,7);
     }
   });
 });
@@ -42,53 +42,55 @@ numberInput.forEach(num => {
 //uses the operatorSelect variable and event bubbling to invoke the desired maths function
 operatorInput.forEach(operator => {
   operator.addEventListener('click', () => {
-    if (operatorSelect == '/') {
+    if (operatorSelect == '/' && secondSum !== '') {
       firstSum = divideFunction(firstSum, secondSum);
-  } else if (operatorSelect == '*') {
+  } else if (operatorSelect == '*' && secondSum !== '') {
       firstSum = multiplyFunction(firstSum, secondSum);
-  } else if (operatorSelect == '-') {
+  } else if (operatorSelect == '-' && secondSum!== '') {
       firstSum = subtractFunction(firstSum, secondSum);
-  } else if (operatorSelect == '+') {
+  } else if (operatorSelect == '+' && secondSum !== '') {
       firstSum = addFunction(firstSum, secondSum);
   }
-    numberDisplay.textContent = firstSum;
+    numberDisplay.textContent = firstSum.toString().slice(0,7);
   });
 });
 
+//toggles current number to positive or negative number
 plusMinusInput.addEventListener('click', () => {
   if (firstSum !== '' && secondSum == '') {
-  firstSum = plusMinusFunction(firstSum);
-  numberDisplay.textContent = firstSum;
+    firstSum = plusMinusFunction(firstSum);
+    numberDisplay.textContent = firstSum.toString().slice(0,7);
   } else {
     secondSum = plusMinusFunction(secondSum);
-    numberDisplay.textContent = secondSum;
+    numberDisplay.textContent = secondSum.toString().slice(0,7);
   }
 });
 
+//invokes the percentage function on the current sum
 percentageInput.addEventListener('click', () => {
   firstSum = percentFunction(firstSum);
-  numberDisplay.textContent = firstSum;
+  numberDisplay.textContent = firstSum.toString().slice(0,7);
 });
 
 //saves the operator clicked into the operatorSelect variable
 divideInput.addEventListener('click', () => {
   operatorSelect = divideInput.textContent;
-    secondSum = '';
+  secondSum = '';
 });
 
 multiplyInput.addEventListener('click', () => {
   operatorSelect = multiplyInput.textContent;
-    secondSum = '';
+  secondSum = '';
 });
 
 subtractInput.addEventListener('click', () => {
   operatorSelect = subtractInput.textContent;
-    secondSum = '';
+  secondSum = '';
 });
 
 addInput.addEventListener('click', () => {
   operatorSelect = addInput.textContent;
-    secondSum = '';
+  secondSum = '';
 });
 
 //uses operator in operatorSelect variable and invokes maths function
@@ -102,32 +104,28 @@ equalsInput.addEventListener('click', () => {
   } else if (operatorSelect == '+' && secondSum !== '') {
       firstSum = addFunction(firstSum, secondSum);
   }
-    numberDisplay.textContent = firstSum;
+    numberDisplay.textContent = firstSum.toString().slice(0,7);
     secondSum = '';
 });
 
 //input the decimal into the sum variables and disables the decimal button if it has already been used in each sum
-// decimalInput.addEventListener('click', () => {
-//   if (!firstSum.includes('.') && !secondSum.includes('.')) {
-//     decimalInput.disabled = false;
-//     firstSum += decimalInput.textContent;
-//     numberDisplay.textContent = firstSum;
-//   } else if (!secondSum.includes('.')) {
-//     decimalInput.disabled = false;
-//     secondSum += decimalInput.textContent;
-//     numberDisplay.textContent = secondSum;
-//   }
-// });
+decimalInput.addEventListener('click', () => {
+  if (!firstSum.toString().includes('.') && secondSum == '' && operatorSelect == '') {
+    firstSum += decimalInput.textContent;
+    numberDisplay.textContent = firstSum.toString().slice(0,7);
+  } else if (!secondSum.toString().includes('.') && secondSum !== '') {
+    secondSum += decimalInput.textContent;
+    numberDisplay.textContent = secondSum.toString().slice(0,7);
+  }
+});
 
 //clears the calculator display on click of C button
 clearInput.addEventListener('click', () => {
   firstSum = '';
   secondSum = '';
   operatorSelect = '';
-  numberDisplay.textContent = '';
+  numberDisplay.textContent = '0';
 });
-
-
 
 
 
